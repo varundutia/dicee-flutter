7637b9d79dd0f107648be5f8e4e43ee4e0956a6d
varundutia <51284086+varundutia@users.noreply.github.com>
2020-12-07 18:51:48 +0530

    first commit
    basic flutter app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'dart:math';
 void main() {
   return runApp(
     MaterialApp(
@@ -15,9 +15,52 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+
+}
+
+class _DicePageState extends State<DicePage> {
+  var random = new Random();
+  var leftDiceNumber=1;
+  var rightDiceNumber=1;
+  void changeState(){
+    setState(() {
+      leftDiceNumber=random.nextInt(5)+1;
+      rightDiceNumber=random.nextInt(5)+1;
+    });
+  }
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: FlatButton(
+                onPressed: () {
+                  changeState();
+                },
+                child: Image.asset('images/dice$leftDiceNumber.png'),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: FlatButton(
+                onPressed: () {
+                  changeState();
+                },
+                child: Image.asset('images/dice$rightDiceNumber.png')
+                ,),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
+
